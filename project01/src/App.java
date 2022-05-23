7094fb80aa37938dd8adf6c2da17163e8729b099
Max Zavaleta Villar <mzavaletav@outlook.com>
2022-05-23 18:49:37 -0500

    Add class Book

--- project01/src/App.java
+++ project01/src/App.java
@@ -1,3 +1,5 @@
+import model.Book;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.printf("Qty args: %d\n", args.length);
@@ -6,5 +8,7 @@ public class App {
             System.out.format("Args[%d]: %s\n", i,string);
             i++;
         }
+        Book book = new Book(1, "100 años de soledad", "Gabriel García Marquez");
+        
     }
 }
